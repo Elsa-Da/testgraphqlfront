9b18d62d12ba408eb4d32e785d6d400b89d9072a
Elsa David <els.david8@gmail.com>
2023-06-01 15:47:59 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AddUser from './components/AddUser'
+import GetUsers from './components/GetUsers'
+import GetUser from './components/GetUser'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const OnChangeTab = (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+        activeTab: string
+    ) => {
+        const tabcontent = document.getElementsByClassName('tabcontent')
+        for (let i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].setAttribute('style', 'display:none')
+        }
+        const tablinks = document.getElementsByClassName('tablinks')
+        for (let i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(' active', '')
+        }
+        document
+            .getElementById(activeTab)
+            ?.setAttribute('style', 'display:block')
+        e.currentTarget.className += ' active'
+    }
+
+    return (
+        <>
+            <div className="tab">
+                <button
+                    className="tablinks active"
+                    onClick={(e) => OnChangeTab(e, 'Users')}
+                >
+                    Users
+                </button>
+                <button
+                    className="tablinks"
+                    onClick={(e) => OnChangeTab(e, 'User')}
+                >
+                    User
+                </button>
+            </div>
+            <div id="Users" className="tabcontent">
+                <h3>Users</h3>
+                <AddUser />
+                <GetUsers />
+            </div>
+
+            <div id="User" className="tabcontent" style={{ display: 'none' }}>
+                <h3>User</h3>
+                <GetUser />
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
